4853badf4bb4c6c30d556bd5b1fe0b8fe3b26a22
Johannes-Lange <71825665+Johannes-Lange@users.noreply.github.com>
2021-01-16 17:33:39 +0100

    progress bar

--- Tools.py
+++ Tools.py
@@ -2,8 +2,10 @@ from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
 from CGAL.CGAL_Kernel import Point_3
 from CGAL.CGAL_Polygon_mesh_processing import Side_of_triangle_mesh
 from CGAL.CGAL_Kernel import ON_BOUNDED_SIDE, ON_BOUNDARY, ON_UNBOUNDED_SIDE
+import pandas as pd
 import math
 import os
+import sys
 
 """
 case: 'cartesian', 'hexdens', 'cylindric'
@@ -12,54 +14,56 @@ cylindric: cylindric distribution along x-axis
 
 
 class PlacePoints:
-    def __init__(self, filename, case, radius):
-        self.radius = radius
+    def __init__(self, filename, case, radius_particle):
+        self.radius = radius_particle
         self.case = case
         # build polyhedron from file
         self.file = os.getcwd() + filename
         self.polyhedron = Polyhedron_3(self.file)
-        assert self.polyhedron.is_closed()
-
+        assert self.polyhedron.is_closed(), 'Polyhedron not closed'
         self.sideof = Side_of_triangle_mesh(self.polyhedron)
 
-        # coordinate ranges x,y,z or x,r,phi
+        # coordinate ranges x,y,z or x,r,phi from off-geometry
         self.range_i = None
         self.range_j = None
         self.range_k = None
-        self.range()
 
         # Points inside
-        self.points = []
+        self.points = []  # store CGAL Point_3
 
         # define points
         if case == 'cartesian':
+            self.range_cart()
             self.cartesian()
         elif case == 'hexdens':
+            self.range_cart()
             self.hexagonal_densest()
         elif case == 'cylindric':
+            self.range_cyl()
             self.cylindric()
         else:
             raise ValueError('Unknown case "', case, '"')
 
-    def range(self):
-        if self.case == 'cartesian' or self.case == 'hexdens':
-            x_range, y_range, z_range = [0., 0.], [0., 0.], [0., 0.]
-            for p in self.polyhedron.points():
-                x_range[0], x_range[1] = min(x_range[0], p.x()), max(x_range[1], p.x())
-                y_range[0], y_range[1] = min(y_range[0], p.y()), max(y_range[1], p.y())
-                z_range[0], z_range[1] = min(z_range[0], p.z()), max(z_range[1], p.z())
-            self.range_i = x_range
-            self.range_j = y_range
-            self.range_k = z_range
-        elif self.case == 'cylindric':
-            x_range, r_range, phi_range = [0., 0.], [0., 0.], [0., 2*math.pi]
-            for p in self.polyhedron.points():
-                x_range[0], x_range[1] = min(x_range[0], p.x()), max(x_range[1], p.x())
-                r_range[1] = max(r_range[1], math.sqrt(p.y()**2+p.z()**2))
-            self.range_i = x_range
-            self.range_j = r_range
-            self.range_k = phi_range
-            # axis + transformation matrix
+        self.point_coordinates = self.create_points()  # Dataframe containing all point-coordinates
+
+    def range_cart(self):  # defines cartesian coordinate range of off-geometry
+        x_range, y_range, z_range = [0., 0.], [0., 0.], [0., 0.]
+        for p in self.polyhedron.points():
+            x_range[0], x_range[1] = min(x_range[0], p.x()), max(x_range[1], p.x())
+            y_range[0], y_range[1] = min(y_range[0], p.y()), max(y_range[1], p.y())
+            z_range[0], z_range[1] = min(z_range[0], p.z()), max(z_range[1], p.z())
+        self.range_i = x_range
+        self.range_j = y_range
+        self.range_k = z_range
+
+    def range_cyl(self):  # defines cylindric coordinate range of off-geometry
+        x_range, r_range, phi_range = [0., 0.], [0., 0.], [0., 2 * math.pi]
+        for p in self.polyhedron.points():
+            x_range[0], x_range[1] = min(x_range[0], p.x()), max(x_range[1], p.x())
+            r_range[1] = max(r_range[1], math.sqrt(p.y() ** 2 + p.z() ** 2))
+        self.range_i = x_range
+        self.range_j = r_range
+        self.range_k = phi_range
 
     def cartesian(self):
         print('\nDistribution: cartesian')
@@ -69,8 +73,10 @@ class PlacePoints:
         nb_points_j = math.ceil(range_diff(self.range_j) / dx) + 1
         nb_points_k = math.ceil(range_diff(self.range_k) / dx) + 1
 
-        print('Number of points to check: ', nb_points_i*nb_points_j*nb_points_k)
+        points_total = nb_points_i*nb_points_j*nb_points_k
+        print('Number of points to check: ', points_total)
 
+        progress = 0
         for i in range(nb_points_i):
             for j in range(nb_points_j):
                 for k in range(nb_points_k):
@@ -80,13 +86,16 @@ class PlacePoints:
                     p = Point_3(px, py, pz)
                     if self.sideof.bounded_side(p) == ON_BOUNDED_SIDE or self.sideof.bounded_side(p) == ON_BOUNDARY:
                         self.points.append(p)
-        print('Number of points inside polyhedron: ', len(self.points))
+                    progress += 1
+                    if progress % 1000 == 0 or progress == points_total:
+                        process(progress, points_total)
+        print('\nNumber of points inside polyhedron: ', len(self.points))
 
     def cylindric(self):
         print('\nDistribution: cylindric')
         print('Radius particle: ', self.radius)
         dx = 2 * self.radius
-        points_to_check = 0
+        points_total = 0
         nb_points_i = math.ceil(range_diff(self.range_i) / dx) + 1  # dx
         nb_points_j = math.ceil(range_diff(self.range_j) / dx) + 1  # dr
 
@@ -99,10 +108,11 @@ class PlacePoints:
                     nb_points_k = math.floor(2*math.pi/dphi) + 1
                 else:
                     nb_points_k = 1
-                points_to_check += nb_points_k
-        print('\nNumber of points to check: ', points_to_check)
+                points_total += nb_points_k
+        print('\nNumber of points to check: ', points_total)
 
         # check points
+        progress = 0
         for i in range(nb_points_i):
             for j in range(nb_points_j):
                 if j != 0:
@@ -120,7 +130,10 @@ class PlacePoints:
                     p = Point_3(px, py, pz)
                     if self.sideof.bounded_side(p) == ON_BOUNDED_SIDE or self.sideof.bounded_side(p) == ON_BOUNDARY:
                         self.points.append(p)
-        print('Number of points inside polyhedron: ', len(self.points))
+                    progress += 1
+                    if progress % 1000 == 0 or progress == points_total:
+                        process(progress, points_total)
+        print('\nNumber of points inside polyhedron: ', len(self.points))
 
     def hexagonal_densest(self):
         print('\nDistribution: hexagonal densest package')
@@ -129,8 +142,10 @@ class PlacePoints:
         nb_points_i = math.ceil(range_diff(self.range_i) / (2*dx)) + 1
         nb_points_j = math.ceil(range_diff(self.range_j) / (math.sqrt(3)*dx)) + 1
         nb_points_k = math.ceil(range_diff(self.range_k) / (2*math.sqrt(6)*dx/3)) + 1
-        print('\nNumber of points to check: ', nb_points_i * nb_points_j * nb_points_k)
+        points_total = nb_points_i * nb_points_j * nb_points_k
+        print('\nNumber of points to check: ', points_total)
 
+        progress = 0
         for i in range(nb_points_i):
             for j in range(nb_points_j):
                 for k in range(nb_points_k):
@@ -140,8 +155,25 @@ class PlacePoints:
                     p = Point_3(px, py, pz)
                     if self.sideof.bounded_side(p) == ON_BOUNDED_SIDE or self.sideof.bounded_side(p) == ON_BOUNDARY:
                         self.points.append(p)
-        print('Number of points inside polyhedron: ', len(self.points))
+                    progress += 1
+                    if progress % 1000 == 0 or progress == points_total:
+                        process(progress, points_total)
+        print('\nNumber of points inside polyhedron: ', len(self.points))
+
+    def create_points(self):
+        out = [[p.x(), p.y(), p.z()] for p in self.points]
+        out = pd.DataFrame(out, columns=['x', 'y', 'z'])
+        return out
+
+    def write_csv(self, filename):
+        self.point_coordinates.to_csv(filename)
 
 
 def range_diff(r):
     return abs(r[1]-r[0])
+
+
+def process(points_comp, points_total):
+    out = '\r{}% --- {} points of {} checked'.format(int(points_comp/points_total*100), points_comp, points_total)
+    sys.stdout.write(out)
+    sys.stdout.flush()
--- test_class.py
+++ test_class.py
@@ -1,9 +1,24 @@
 import Tools
+import time
 import pandas as pd
+t1 = time.time()
+x = Tools.PlacePoints('/Vortex_Tube.off', 'cartesian', 2e-4)
+print(time.time()-t1)
+"""
+t1 = time.time()
+x = Tools.PlacePoints('/Vortex_Tube.off', 'cartesian', 4e-4)
+x.write_csv('points_inside_cart_4.csv')
+t2 = time.time()
 
-x = Tools.PlacePoints('/bunny.off', 'cylindric', 0.01)
+x = Tools.PlacePoints('/Vortex_Tube.off', 'cartesian', 2e-4)
+x.write_csv('points_inside_cart_2.csv')
+t3 = time.time()
 
-out = [[p.x(), p.y(), p.z()] for p in x.points]
-out = pd.DataFrame(out, columns=['x', 'y', 'z'])  # 45600
+x = Tools.PlacePoints('/Vortex_Tube.off', 'cartesian', 1e-4)
+x.write_csv('points_inside_cart_1.csv')
+t4 = time.time()
 
-out.to_csv('points_inside_cylindric.csv')
+time = [t2-t1, t3-t2, t4-t3]
+time = pd.DataFrame(time)
+time.to_csv('cart.csv')
+"""
